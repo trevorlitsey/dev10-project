e6b2aa3fedab9332e25216ac54a841af1b8cfb47
Trevor Litsey <trevorlitsey@gmail.com>
2018-12-01 16:47:38 -0600

    implement restaurant 3

--- resturant-site-3/scripts/contactUs.js
+++ resturant-site-3/scripts/contactUs.js
@@ -1,1 +1,29 @@
-console.log('contact us');
+const form = document.querySelector('form');
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const [name, email, _, reason] = e.target;
+
+  const alertMessage = document.createElement('div');
+
+  const isValid = name.value && email.value && reason.value;
+
+  if (isValid) {
+    alertMessage.classList.add('alert', 'alert-success');
+    alertMessage.innerText =
+      'Thank your for contacting us! We will be in touch soon.';
+  } else {
+    alertMessage.classList.add('alert', 'alert-danger');
+    alertMessage.innerText =
+      'Please input name, email, and reason for inquiry!';
+  }
+
+  form.insertBefore(alertMessage, form.childNodes[0]);
+
+  if (isValid) {
+    form.reset();
+  }
+}
+
+form.addEventListener('submit', handleSubmit);
